398c824d722cba6a64ff408cd73767bd81326a09
Mario Arturo Sanchez Ruelas <marioarturosanchez@gmail.com>
2023-10-23 14:30:12 -0600

    Intial Commit

---
+++ src/AuctionView.java
@@ -0,0 +1,32 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+
+public class AuctionView{
+    private JFrame userWindow;
+    private JPanel panel;
+    JTextField user;
+    JTextField product;
+    JTextField initialPrice;
+    JTextField amount;
+
+    public AuctionView(){
+        JFrame.setDefaultLookAndFeelDecorated( true );
+
+        userWindow = new JFrame("Auction Client Program");
+
+        panel = new JPanel(new GridLayout(0, 3));
+        userWindow.setContentPane(panel);
+
+        panel.add();
+
+
+
+
+        userWindow.setSize( 400, 400 );
+	    userWindow.setVisible( true );
+    }
+
+
+}
